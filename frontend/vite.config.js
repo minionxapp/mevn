f3f4d80d19bf70efbb05a4166455b2dfcb85031e
Mugi <minionxapp@gmail.com>
2025-04-10 22:48:36 +0700

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig ,loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default({mode})=>{
-  process.env = {...process.env, ...loadEnv(mode,process.cwd())}
+  
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
   return defineConfig({
     plugins: [
       vue(),
@@ -15,7 +15,6 @@ export default({mode})=>{
   
     server: {
       proxy: {
-        
         // '/foo': 'http://localhost:4567',
         '/api': {
           target: process.env.VITE_API_URL,
