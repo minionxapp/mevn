deea14fb316034d2a7a0ebad1d929389f48ce8a3
Mugi <minionxapp@gmail.com>
2025-04-10 20:04:07 +0700

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig ,loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-
-  server: {
-    proxy: {
-      
-      // '/foo': 'http://localhost:4567',
-      '/api': {
-        target: 'https://backendmevn.vercel.app/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
-  },
-
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default({mode})=>{
+  process.env = {...process.env, ...loadEnv(mode,process.cwd())}
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+  
+    server: {
+      proxy: {
+        
+        // '/foo': 'http://localhost:4567',
+        '/api': {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      }
     },
-  },
-})
+  
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+  })
+} 
