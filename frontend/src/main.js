a446c63df4fe2897091fd5b908e7806b9308a9f4
Mugi <minionxapp@gmail.com>
2025-04-10 21:01:09 +0700

    deploy

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -26,9 +26,9 @@ const pinia = createPinia()
 //     }
 // });
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*"); // Or specify a specific origin
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Origin", "*"); // Or specify a specific origin
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
   });
 app.use(PrimeVue)
